2a338682e3f6301a79f565761a0bc6c49a3c0a21
ftiosso <ftiosso@gmail.com>
2024-07-16 01:00:02 -0300

    Inclusão do script para criação do banco de dados consultorio: [Estrutura] + [Usuário Administrador]

---
+++ script.sql
@@ -0,0 +1,54 @@
+create database consultorio;
+GO
+
+USE consultorio;
+GO
+
+CREATE TABLE medico(
+	id int IDENTITY(1,1) NOT NULL,
+	crm char(9) NOT NULL,
+	nome varchar(100) NULL,
+	CONSTRAINT pk_medico PRIMARY KEY (id)
+);
+
+CREATE TABLE paciente(
+	codigo int IDENTITY(1,1) NOT NULL,
+	nome varchar(100) NOT NULL,
+	datanascimento date NOT NULL,
+	CONSTRAINT pk_codigo PRIMARY KEY (codigo)
+);
+
+CREATE TABLE medicamento(
+	id int IDENTITY(1,1) NOT NULL,
+	nome varchar(100) NOT NULL,
+	datafabricacao date NOT NULL,
+	datavencimento date NULL,
+	CONSTRAINT pk_medicamento PRIMARY KEY (id)
+);
+
+CREATE TABLE consulta(
+	id int IDENTITY(1,1) NOT NULL,
+	data datetime NULL,
+	idmedico int NOT NULL,
+	codigopaciente int NOT NULL,
+ 	CONSTRAINT pk_consulta PRIMARY KEY (id),
+	CONSTRAINT fk_consulta_medico FOREIGN KEY (idmedico) references medico (id),
+ 	CONSTRAINT fk_consulta_paciente FOREIGN KEY (codigopaciente) references paciente (codigo)
+);
+
+CREATE TABLE usuario(
+	id int IDENTITY(1,1) NOT NULL,
+	nome varchar(100) NOT NULL,
+	email varchar(100) NOT NULL,
+	senha char(32) NOT NULL,
+	CONSTRAINT pk_usuario PRIMARY KEY (id),
+	CONSTRAINT uk_usuario_email UNIQUE (email)
+);
+
+CREATE TABLE especialidade(
+	id int not null identity (1,1),
+	nome varchar(100) not null,
+	constraint pk_especialidade primary key (id)
+);
+
+insert into usuario (nome, email, senha) values ('Administrador', 'adm@adm.br', '202CB962AC59075B964B07152D234B70');
